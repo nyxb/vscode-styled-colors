0ff443b1c6d1fd6285fdd7dc1a1ea6ae66572e9e
nyxb <contact@nyxb.xyz>
2023-06-19 18:03:28 +0200

    ✨ feat(cache-manager.ts): add CacheManager class to manage dirty and saved decorations cache ✨ feat(editor-manager.ts): add EditorManager class to manage editor's decorations ✨ feat(extractor-mixin.ts): add Extractor class to manage strategies and enable them

---
+++ src/lib/editor-manager.ts
@@ -0,0 +1,46 @@
+import type { TextEditor } from 'vscode'
+import type { IDecoration } from './util/color-util'
+
+class EditorManager {
+   /**
+   * Run through a list of decorations to generate editor's decorations
+   *
+   * @static
+   * @param {TextEditor} editor
+   * @param {Map<number, IDecoration[]>} decorations
+   * @param {number[]} skipLines
+   * @returns
+   * @memberof EditorManager
+   */
+   public static decorate(editor: TextEditor, decorations: Map<number, IDecoration[]>, skipLines: number[]): void {
+      const it = decorations.entries()
+      let tmp = it.next()
+      while (!tmp.done) {
+         const line = tmp.value[0]
+         const deco: IDecoration[] = tmp.value[1]
+         if (!skipLines.includes(line))
+            this.decorateOneLine(editor, deco, line)
+
+         tmp = it.next()
+      }
+   }
+
+   // Decorate editor's decorations for one line
+   /**
+   * Generate decorations in one line for the selected editor
+   *
+   * @static
+   * @param {TextEditor} editor
+   * @param {IDecoration[]} decorations
+   * @param {number} line
+   * @memberof EditorManager
+   */
+   public static decorateOneLine(editor: TextEditor, decorations: IDecoration[], line: number): void {
+      decorations.forEach((decoration: IDecoration) => {
+         if (decoration.shouldGenerateDecoration() === true)
+            editor.setDecorations(decoration.decoration, [decoration.generateRange(line)])
+      })
+   }
+}
+
+export default EditorManager
