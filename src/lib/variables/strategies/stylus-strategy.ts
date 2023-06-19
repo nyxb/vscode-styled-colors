9844a8e26301fe0388ea62968926011ccc350e0f
nyxb <contact@nyxb.xyz>
2023-06-19 18:08:58 +0200

    ✨ feat(stylus-strategy.ts): add new file to register a new strategy for extracting variables from Stylus files. The strategy uses regular expressions to extract variable declarations and uses.

---
+++ src/lib/variables/strategies/stylus-strategy.ts
@@ -0,0 +1,23 @@
+import VariablesExtractor from '../variables-extractor'
+import { EOL } from '../../util/regexp'
+import VariableStrategy from './__strategy-base'
+
+export const REGEXP = new RegExp(`(^|(?::|=)\\s*)((?:[\\-]*[$a-z_][\\-_\\d]*)+)(?!=)${EOL}`, 'gi')
+export const REGEXP_ONE = new RegExp(`(^|(?::|=)\\s*)((?:[\\-]*[$a-z_][\\-_\\d]*)+)(?!=)${EOL}`, 'i')
+export const DECLARATION_REGEXP = new RegExp(`(?:(^(?:\\$|(?:[\\-_$]+[a-z\\d]+)|(?:[^\\d||\\-|@]+))(?:[_a-z\\d][\\-]*)*))\\s*=${EOL}`, 'gi')
+
+const RegexpExtractor = {
+   getVariableNameFromDeclaration(match: RegExpExecArray): string {
+      return (match[1] || match[2]).trim()
+   },
+
+   getVariableNameFromUses(match: RegExpExecArray): string[] {
+      return [match[2].trim(), null, match[1]]
+   },
+
+   getVariableNameFromUse(match: RegExpMatchArray): string {
+      return match[2].trim()
+   },
+}
+
+VariablesExtractor.registerStrategy(new VariableStrategy('STYLUS', DECLARATION_REGEXP, REGEXP, REGEXP_ONE, RegexpExtractor))
