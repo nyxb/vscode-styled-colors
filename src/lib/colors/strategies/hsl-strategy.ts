18a585e4e9bebb6cc9d6890bbe18ba41bb131e17
nyxb <contact@nyxb.xyz>
2023-06-19 18:06:02 +0200

    🚀 feat(hsl-strategy.ts): add support for HSL and HSLA color formats to extract and convert them to RGB and RGBA respectively.

---
+++ src/lib/colors/strategies/hsl-strategy.ts
@@ -0,0 +1,38 @@
+import ColorExtractor from '../color-extractor'
+import { convertHslaToRgba } from '../../util/color-util'
+import { ALPHA, DOT_VALUE, EOL } from '../../util/regexp'
+import Color from './../color'
+import ColorStrategy from './__strategy-base'
+
+const R_HUE = `\\d*${DOT_VALUE}?`
+const R_SATURATION = `(?:\\d{1,3}${DOT_VALUE}?|${DOT_VALUE})%`
+const R_LUMINANCE = R_SATURATION
+
+export const REGEXP = new RegExp(`((?:hsl\\(\\s*${R_HUE}\\s*,\\s*${R_SATURATION}\\s*,\\s*${R_LUMINANCE}\\s*\\))|(?:hsla\\(\\s*${R_HUE}\\s*,\\s*${R_SATURATION}\\s*,\\s*${R_LUMINANCE}\\s*,\\s*${ALPHA}\\s*\\)))${EOL}`, 'gi')
+export const REGEXP_ONE = new RegExp(`^((?:hsl\\(\\s*${R_HUE}\\s*,\\s*${R_SATURATION}\\s*,\\s*${R_LUMINANCE}\\s*\\))|(?:hsla\\(\\s*${R_HUE}\\s*,\\s*${R_SATURATION}\\s*,\\s*${R_LUMINANCE}\\s*,\\s*${ALPHA}\\s*\\)))${EOL}`, 'i')
+// export const REGEXP_ONE = /^((?:hsl\(\d*\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\))|(?:hsla\(\d*\s*,\s*(?:\d{1,3}%\s*,\s*){2}(?:[0-1]|1\.0|[0](?:\.\d+){0,1}|(?:\.\d+))\)))(?:$|"|'|,| |;|\)|\r|\n)/i;
+
+/**
+ * @private
+ * @param {any} value An hsl(a) color string (`hsl(10, 1%, 1%)`)
+ * @returns {number[]} The colors h,s,l,a values
+ *
+ * @memberof HSLColorExtractor
+ */
+function extractHSLValue(value) {
+   const [h, s, l, a]: number[] = value.replace(/hsl(a){0,1}\(/, '').replace(/\)/, '').replace(/%/g, '').split(/,/gi).map(c => parseFloat(c))
+   return [h, s, l, a]
+}
+
+function getColor(match: RegExpExecArray): Color {
+   const value = match[0]
+   const [h, s, l, a] = extractHSLValue(value)
+   if (s <= 100 && l <= 100) {
+      const [r, g, b] = convertHslaToRgba(h, s, l, a)
+      return new Color(match[1], match.index, [r, g, b], a)
+   }
+   return null
+}
+const strategy = new ColorStrategy('HSL', REGEXP, REGEXP_ONE, getColor)
+ColorExtractor.registerStrategy(strategy)
+export default strategy
